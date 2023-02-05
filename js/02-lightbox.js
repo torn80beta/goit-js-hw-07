be9aa8fc6a5c0922d2c09c30beb6fae393e7d44b
Alex <103459735+torn80beta@users.noreply.github.com>
2023-02-05 23:22:44 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
-console.log(galleryItems);
-
 const galleryEl = document.querySelector('.gallery');
 const galleryMarkup = galleryItems.map(({preview, original, description}) => 
     `<a class="gallery__item" href="${original}">
